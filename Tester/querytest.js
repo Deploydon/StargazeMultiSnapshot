dce40f46ab14ef1fbe1f8a0fd99786d2d04513fb
Deploydon <deploydon@gmail.com>
2022-10-24 21:27:46 -0400

    Added correct/cleaned querytest script

--- Tester/querytest.js
+++ Tester/querytest.js
@@ -5,45 +5,15 @@ const RPC = "https://rpc.elgafar-1.stargaze-apis.com/";
 
 var CONTRACT_MULTI = "stars18eupq2qjpygftyl488yzkr06s3kmw6j8zs9utnh7kyfk9tck0swsxkn8cj";
 
+var COLLECTION_SG721 = "stars1ee4a3ad6lmc3ckvuuzlwk4vsyu7g7d7khtck07tsa8wgavapqarsvycuw4";
 
-
-
-var COLLECTION_SG721 = "stars1ee4a3ad6lmc3ckvuuzlwk4vsyu7g7d7khtck07tsa8wgavapqarsvycuw4"; 
+const START = 1;
+const END = 30;
 async function main() {
-    var ID = 1;
     const client = await cosmwasm.CosmWasmClient.connect(RPC);
-  // var collectionInfo = await client.queryContractSmart(COLLECTION, { config: {} });
-   //console.log(collectionInfo);
-  //  return;
-  //var collectionTokenInfo = await client.queryContractSmart(COLLECTION, { owner_of: {token_id:"41", include_expired: false} });
-   // console.log(collectionTokenInfo);
-  //  return;
-    const tokenInfo = await client.queryContractSmart(CONTRACT_MULTI, { collection_owners: { collection: COLLECTION_SG721 , start:30, end:45 } });
-    console.log(tokenInfo);
-
+    const tokenOwners = await client.queryContractSmart(CONTRACT_MULTI, { collection_owners: { collection: COLLECTION_SG721, start: START, end: END } });
+    console.log(tokenOwners);
 }
-
-
-
-
-async function getClient() {
-    const gasPrice = cosmwasm.GasPrice.fromString('0ustars');
-    const wallet = await cosmwasm.DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
-        prefix: 'stars',
-    });
-    const [{ address, pubkey }] = await wallet.getAccounts();
-
-    myAddress = address;
-    console.log(myAddress);
-
-    return await cosmwasm.SigningCosmWasmClient.connectWithSigner(
-        RPC,
-        wallet,
-        { gasPrice }
-    );
-}
-
-
 
 
 main();
